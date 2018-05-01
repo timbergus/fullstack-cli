a5ccf7e3b912a9d02e2fe275b83db08e199adab1
Gustavo Muñoz <timbergus@gmail.com>
2018-05-01 10:00:28 +0200

    Add missing library.

--- modules/front/common/templates/jest.config.js
+++ modules/front/common/templates/jest.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-  testRegex: '/*.test.js$',
-  verbose: false,
   notify: true,
+  testRegex: '/*.test.js$',
   collectCoverage: true,
+  coverageReporters: ['lcov'],
+  coverageDirectory: '__reports__/test-coverage',
   collectCoverageFrom: [
     'src/**/*.(js|jsx)'
   ],
-  coverageDirectory: '__reports__/test-coverage',
-  coverageReporters: ['lcov'],
   moduleNameMapper: {
     '^reducers(.*)$': '<rootDir>/src/app/reducers/$1',
     '^components(.*)$': '<rootDir>/src/app/components/$1'
