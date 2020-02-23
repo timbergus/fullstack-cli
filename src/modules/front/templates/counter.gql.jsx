2206870e59a286c7b92408a595ae1f7ed0f0930a
Gustavo Muñoz <timbergus@gmail.com>
2020-02-23 20:52:36 +0100

    perf(front): add new GraphQL hooks
    Add new GraphQL hooks and remove old components Query and Mutation.

--- src/modules/front/templates/counter.gql.jsx
+++ src/modules/front/templates/counter.gql.jsx
@@ -1,77 +1,80 @@
 // @flow
 
 import React from 'react';
-import { Query, Mutation } from 'react-apollo';
-{{# material-ui }}
+import { useQuery, useMutation } from '@apollo/react-hooks';
+{{#material-ui}}
 
 import Button from '@material-ui/core/Button';
-{{/ material-ui }}
+{{/material-ui}}
 
 import { GET_COUNTER, MODIFY_COUNTER } from '../apollo/queries';
 
-export default () => (
-  <>
-    <h1>Welcome to the counter!</h1>
-    <Query query={GET_COUNTER}>
-      {
-        ({ data }) => <h2>{data?.counter?.value}</h2>
-      }
-    </Query>
-    <Mutation mutation={MODIFY_COUNTER}>
-      {
-        (modifyCounter) => (
-          {{#material-ui}}
-          <>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => modifyCounter({
-                variables: {
-                  amount: 1,
-                },
-              })}
-            >
-              Increment
-            </Button>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => modifyCounter({
-                variables: {
-                  amount: -1,
-                },
-              })}
-            >
-              Decrement
-            </Button>
-          </>
-          {{/material-ui}}
-          {{^material-ui}}
-          <>
-            <button
-              type="button"
-              onClick={() => modifyCounter({
-                variables: {
-                  amount: 1,
-                },
-              })}
-            >
-              Increment
-            </button>
-            <button
-              type="button"
-              onClick={() => modifyCounter({
-                variables: {
-                  amount: -1,
-                },
-              })}
-            >
-              Decrement
-            </button>
-          </>
-          {{/material-ui}}
-        )
-      }
-    </Mutation>
-  </>
-);
+export default () => {
+  const { loading, error, data } = useQuery(GET_COUNTER);
+  const [modifyCounter] = useMutation(MODIFY_COUNTER);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error...</p>;
+  }
+
+  return (
+    <>
+      <h1>Welcome to the counter!</h1>
+      <h2>{data?.counter?.value}</h2>
+      {{#material-ui}}
+      <>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => modifyCounter({
+            variables: {
+              amount: 1,
+            },
+          })}
+        >
+          Increment
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => modifyCounter({
+            variables: {
+              amount: -1,
+            },
+          })}
+        >
+          Decrement
+        </Button>
+      </>
+      {{/material-ui}}
+      {{^material-ui}}
+      <>
+        <button
+          type="button"
+          onClick={() => modifyCounter({
+            variables: {
+              amount: 1,
+            },
+          })}
+        >
+          Increment
+        </button>
+        <button
+          type="button"
+          onClick={() => modifyCounter({
+            variables: {
+              amount: -1,
+            },
+          })}
+        >
+          Decrement
+        </button>
+      </>
+      {{/material-ui}}
+    </>
+  );
+};
